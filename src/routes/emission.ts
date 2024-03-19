f7e4f535a43d4740237050880d502437af2b0280
rasm445f <90387951+rasm445f@users.noreply.github.com>
2024-03-19 19:10:42 +0100

    5 done

--- src/routes/emission.ts
+++ src/routes/emission.ts
@@ -3,6 +3,7 @@ import { getTotalEmissionsByCity } from "../functions/getTotalEmissionsByCity";
 import { getCitiesByStatusType } from "../functions/getCitiesByStatusType";
 import { getAvgEmissionForC40AndNonC40 } from "../functions/getAvgEmissionForC40AndNonC40";
 import { getCityEmissionTargets } from "../functions/getCityEmissionTargets";
+import { getCitiesWithEmissionsRanking } from "../functions/getCitiesWithEmissionsRanking";
 
 interface Params {
   cityName: string;
@@ -13,68 +14,99 @@ interface Params {
 
 export async function emissionRoutes(fastify: FastifyInstance) {
   // 1
-  fastify.get("/total/:cityName", async function (request: FastifyRequest<{ Params: Params }>, reply: FastifyReply) {
-    try {
-      const city = request.params.cityName as string;
-      return await getTotalEmissionsByCity(city.trim());
-    } catch (error) {
-      fastify.log.error(error);
-      reply.code(500).send({ error: "Failed getting total emissions. Please try again later." });
+  fastify.get(
+    "/total/:cityName",
+    async function (
+      request: FastifyRequest<{ Params: Params }>,
+      reply: FastifyReply
+    ) {
+      try {
+        const city = request.params.cityName as string;
+        return await getTotalEmissionsByCity(city.trim());
+      } catch (error) {
+        fastify.log.error(error);
+        reply.code(500).send({
+          error: "Failed getting total emissions. Please try again later.",
+        });
+      }
     }
-  });
+  );
 
   // 2
-  fastify.get("/status/:statusType", async function (request: FastifyRequest<{ Params: Params }>, reply: FastifyReply) {
-    try {
-      const statusType = request.params.statusType;
-      return await getCitiesByStatusType(statusType);
-    } catch (error: any) {
-      fastify.log.error(error);
-      reply.code(500).send({ error: error.message });
+  fastify.get(
+    "/status/:statusType",
+    async function (
+      request: FastifyRequest<{ Params: Params }>,
+      reply: FastifyReply
+    ) {
+      try {
+        const statusType = request.params.statusType;
+        return await getCitiesByStatusType(statusType);
+      } catch (error: any) {
+        fastify.log.error(error);
+        reply.code(500).send({ error: error.message });
+      }
     }
-  });
+  );
 
   // 3
-  fastify.get("/avg", async function (request: FastifyRequest, reply: FastifyReply) {
-    try {
-      return await getAvgEmissionForC40AndNonC40();
-    } catch (error: any) {
-      fastify.log.error(error);
-      reply.code(500).send({ error: "Failed getting emissions. Please try again later." });
+  fastify.get(
+    "/avg",
+    async function (request: FastifyRequest, reply: FastifyReply) {
+      try {
+        return await getAvgEmissionForC40AndNonC40();
+      } catch (error: any) {
+        fastify.log.error(error);
+        reply
+          .code(500)
+          .send({ error: "Failed getting emissions. Please try again later." });
+      }
     }
-  });
+  );
 
   // 4
-  fastify.get("/targets/:cityName", async function (request: FastifyRequest<{ Params: Params }>, reply: FastifyReply) {
-    try {
-      const city = request.params.cityName;
-      return await getCityEmissionTargets(city);
-    } catch (error: any) {
-      fastify.log.error(error);
-      reply.code(500).send({ error: error.message });
+  fastify.get(
+    "/targets/:cityName",
+    async function (
+      request: FastifyRequest<{ Params: Params }>,
+      reply: FastifyReply
+    ) {
+      try {
+        const city = request.params.cityName;
+        return await getCityEmissionTargets(city);
+      } catch (error: any) {
+        fastify.log.error(error);
+        reply.code(500).send({ error: error.message });
+      }
     }
-  });
+  );
 
-  //   // 5
-  //   fastify.get("/ranked/:sortBy?", async function (request: FastifyRequest<{ Params: Params }>, reply: FastifyReply) {
-  //     try {
-  //       if (!request.params.sortBy) {
-  //         const data = await getCitiesWithEmissionsRanking();
-  //         return data;
-  //       }
+  // 5
+  fastify.get(
+    "/ranked/:sortBy?",
+    async function (
+      request: FastifyRequest<{ Params: Params }>,
+      reply: FastifyReply
+    ) {
+      try {
+        if (!request.params.sortBy) {
+          const data = await getCitiesWithEmissionsRanking("DESC");
+          return data;
+        }
 
-  //       const sortBy = request.params.sortBy.toUpperCase();
-  //       if (sortBy !== "ASC" && sortBy !== "DESC") {
-  //         throw new Error("Invalid sorting type. Please use 'ASC' or 'DESC'");
-  //       }
+        const sortBy = request.params.sortBy.toUpperCase();
+        if (sortBy !== "ASC" && sortBy !== "DESC") {
+          throw new Error("Invalid sorting type. Please use 'ASC' or 'DESC'");
+        }
 
-  //       const data = await getCitiesWithEmissionsRanking(sortBy);
-  //       return data;
-  //     } catch (error: any) {
-  //       fastify.log.error(error);
-  //       reply.code(500).send({ error: error.message });
-  //     }
-  //   });
+        const data = await getCitiesWithEmissionsRanking(sortBy);
+        return data;
+      } catch (error: any) {
+        fastify.log.error(error);
+        reply.code(500).send({ error: error.message });
+      }
+    }
+  );
 
   //   // 6
   //   fastify.get("/cities", async function (request: FastifyRequest<{ Params: Params }>, reply: FastifyReply) {
